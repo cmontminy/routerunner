207e144b63c1b233d56e889f8299aad97c32e867
paigeg3 <66644490+paigeg3@users.noreply.github.com>
2022-03-10 16:41:55 -0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Route Runner
+//
+//  Created by Colette Montminy on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+
+    
+    @IBAction func registerButton(_ sender: Any) {
+        //takes to register page
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
