b4e01e2ac2d0fe8c6fb4e3c2059bb697e0f22196
paigeg3 <paigegibson333@utexas.edu>
2022-03-22 22:02:01 -0500

    profile page

---
+++ Route Runner/Profile Screen/ProfileCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Route Runner
+//
+//  Created by Paige Gibson on 3/22/22.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var runImage: UIImageView!
+}
--- Route Runner/Profile Screen/ProfileViewController.swift
+++ Route Runner/Profile Screen/ProfileViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+public let dummyData = ["sample0", "sample1", "sample2", "sample3", "sample4", "sample5"]
 
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+@IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var numRuns: UILabel!
+    @IBOutlet weak var numMiles: UILabel!
+    @IBOutlet weak var numSPs: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        profilePicture.image = UIImage(named: "profilepictureRR")
+        startObserving(&UserInterfaceStyleManager.shared)
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! ProfileCollectionViewCell
+        let row = indexPath.row
+        cell.runImage?.image = UIImage(named: dummyData[row])
+        cell.runImage?.layer.cornerRadius = 8.0
+        cell.runImage?.layer.masksToBounds = true
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        // add the code here to perform action on the cell
+        //when run is clicked on, run detials will be shown, similar to past run in history
+        let cell = collectionView.cellForItem(at: indexPath) as? ProfileCollectionViewCell
     }
     
 
