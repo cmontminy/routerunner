e7070214675544790a90088734c36c2e3cb629a5
Colette Montminy <cmontminy@utexas.edu>
2022-03-22 22:09:21 -0500

    add comments for observer files

--- Route Runner/Observers/StyledNavigationController.swift
+++ Route Runner/Observers/StyledNavigationController.swift
@@ -1,9 +1,12 @@
 //
 //  StyledNavigationController.swift
-//  Route Runner
+//  Project: Route Runner
+//  EID: crm4772
+//  Course: CS371L
 //
 //  Created by Colette Montminy on 3/21/22.
 //
+//  From Lee Kah Seng - https://swiftsenpai.com/development/implement-in-app-dark-mode-using-swift-observation-protocols/
 
 import UIKit
 
