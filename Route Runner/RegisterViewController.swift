098bcfe9ee21e4c4d028bda686e6b9642aa6fb54
paigeg3 <paigegibson333@utexas.edu>
2022-04-08 10:30:45 -0500

    skill level action sheet

--- Route Runner/RegisterViewController.swift
+++ Route Runner/RegisterViewController.swift
@@ -9,13 +9,54 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var selectSkillButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func selectSkillButtonPressed(_ sender: Any) {
+        let controller = UIAlertController(
+            title: "Select Skill Level",
+            message: "Choose Level",
+            preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {
+            (action) in print("Cancel action")
+        })
+        controller.addAction(cancelAction)
+        
+        let beginnerAction = UIAlertAction(title: "Beginner", style: .default, handler: {
+            (action) in print("Beginner selected")
+            self.selectSkillButton.titleLabel?.text = "Beginner"
+        })
+        controller.addAction(beginnerAction)
+        
+        let intermediateAction = UIAlertAction(title: "Intermediate", style: .default, handler: {
+            (action) in print("Intermediate selected")
+            self.selectSkillButton.titleLabel?.text = "Intermediate"
+        })
+        controller.addAction(intermediateAction)
+        
+        let expertAction = UIAlertAction(title: "Expert", style: .default, handler: {
+            (action) in print("Expert selected")
+            self.selectSkillButton.titleLabel?.text = "Expert"
+        })
+        controller.addAction(expertAction)
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
