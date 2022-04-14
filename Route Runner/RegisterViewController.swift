4c2ddf1c3831901204103611b9b76f8f8ebf77f0
paigeg3 <paigegibson333@utexas.edu>
2022-04-14 11:30:27 -0500

    added validation functionality to register page

--- Route Runner/RegisterViewController.swift
+++ Route Runner/RegisterViewController.swift
@@ -18,7 +18,6 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
@@ -55,7 +54,65 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func registerButtonPressed(_ sender: Any) {
+        //validate fields
+        let error = validateFields()
+        if(error != nil){
+            //error has occured, display error text
+            let errorController = UIAlertController(
+                title: "Error",
+                message: "\(error!)",
+                preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            errorController.addAction(OKAction)
+            present(errorController, animated: true, completion: nil)
+            return
+        }
+        //create user
+        //transition to home screen
     }
+    
+    //check fields and validates that all data is correct
+    //if correct, return nil
+    //if incorrect, return error message
+    func validateFields() -> String? {
+        //check that all fields are filled in
+        if((firstNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "") || (lastNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ) || (emailField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "") || (passwordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "") || (confirmPasswordField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "")){
+            return "Please fill in all fields"
+        }
+        //check if skill level has been selected
+        //if skill level has been selected, then the title of the button should
+        //be chnaged to reflect the chosen skill level
+        if(selectSkillButton.titleLabel?.text == "Select Skill Level"){
+            return "Please select skill level"
+        }
+        //check if email is valid
+        if(!isValidEmail(emailField.text!)){
+            return "Please enter a valid email"
+        }
+        //check if password is valid
+        if(!isValidPassword(passwordField.text!)){
+            return "Please enter a valid password of at least 6 characters"
+        }
+        //check if confirm password field matches password field
+        
+        
+        
+        return nil
+    }
+    
+    func isValidPassword(_ password: String) -> Bool {
+      let minPasswordLength = 6
+      return password.count >= minPasswordLength
+    }
+    func isValidEmail(_ email: String) -> Bool {
+      let emailRegEx =
+            "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+      let emailPred = NSPredicate(format:"SELF MATCHES %@",
+            emailRegEx)
+      return emailPred.evaluate(with: email)
+    }
+    
+
     
     /*
     // MARK: - Navigation
