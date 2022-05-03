912d645c528bba6c2c1106a4f49052c42d7d9f2b
Sriram Alagappan <sriram.v.alagappan@gmail.com>
2022-05-03 08:54:30 -0500

    load routes dynamically for home page

--- Route Runner/Home Screen/HomeViewController.swift
+++ Route Runner/Home Screen/HomeViewController.swift
@@ -9,30 +9,85 @@ import UIKit
 import Firebase
 import FirebaseFirestoreSwift
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let cellIdentifier = "RouteCell";
+    var routes:[RunData] = [];
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         startObserving(&UserInterfaceStyleManager.shared)
-        createDummyRunFirebase()
+        tableView.delegate = self
+        tableView.dataSource = self
+        getAllRunsFirebase()
+        self.tableView.reloadData() // after loading routes, rebuild table
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RouteCell
+        let row = indexPath.row
+        cell.titleLabel.text = (routes[row].name)
+        cell.distanceLabel.text = String(routes[row].distance) + " mi"
+        cell.timeLabel.text = String(routes[row].time) + " min"
+        cell.routeImage.image = routes[row].image
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return routes.count
+    }
+    
+    // Get all runs from firebase
+    func getAllRunsFirebase() {
+        let db = Firestore.firestore()
+        self.showSpinner(onView: self.view) // show spinner
+        db.collection("runs").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+                self.removeSpinner() // remove spinner from screen
+            } else {
+                for document in querySnapshot!.documents {
+                    // parse data and get properties
+                    let data = document.data()
+                    let name: String = data["name"] as? String ?? "(No Name)"
+                    let distance: Double = data["distance"] as? Double ?? 0.0
+                    let time: Int = data["time"] as? Int ?? 0
+                    let points: Int = data["points"] as? Int ?? 0
+                    
+                    print(name, distance, time)
+                    
+                    // create route and append to list of all routes
+                    let curRoute = RunData(name: name, image: UIImage(named: "sample3"), date: Date(), distance: distance, points: points, time: time)
+                    self.routes.append(curRoute)
+                }
+                self.tableView.reloadData() // after loading routes, rebuild table
+                self.removeSpinner() // remove spinner from screen
+            }
+        }
     }
     
     // Create a dummy run document on page load for firebase testing
-    func createDummyRunFirebase() {
-        let db = Firestore.firestore()
-        let docRef = db.collection("runs").document("sample")
-
-        docRef.getDocument(as: RunData.self) { result  in
-            switch result {
-            case .success(let run):
-                print("Run: \(run.name)")
-            case .failure(let error):
-                print("Error decoding run: \(error)")
-            }
-        }
-        try? db.collection("runs").addDocument(from: RunData(name: "testing", image: nil, date: Date(), distance: 123.4, points: 5, time: 8))
-    }
+//    func createDummyRunFirebase() {
+//        let db = Firestore.firestore()
+//        let docRef = db.collection("runs").document("sample")
+//
+//        docRef.getDocument(as: RunData.self) { result  in
+//            switch result {
+//            case .success(let run):
+//                print("Run: \(run.name)")
+//            case .failure(let error):
+//                print("Error decoding run: \(error)")
+//            }
+//        }
+//        try? db.collection("runs").addDocument(from: RunData(name: "testing", image: nil, date: Date(), distance: 123.4, points: 5, time: 8))
+//    }
     
     // to run anytime the view appears on the screen
     override func viewWillAppear(_ animated: Bool) {
---
+++ Route Runner/Home Screen/RouteCell.swift
@@ -0,0 +1,17 @@
+//
+//  RouteCell.swift
+//  Route Runner
+//
+//  Created by Sriram Alagappan on 5/2/22.
+//
+
+import Foundation
+
+import UIKit
+
+class RouteCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var routeImage: UIImageView!
+}
